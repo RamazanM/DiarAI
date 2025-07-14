10498aea99472dec761524fc809735ceaa79d7ad
RamazanM <r.ramazanmutlu@gmail.com>
2025-07-14 23:00:46 +0300

    Initial commit Dagger + KSP has been implemented

---
+++ app/src/main/java/com/ramazanmutlu/diarai/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.ramazanmutlu.diarai
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ramazanmutlu.diarai.ui.theme.DiarAiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiarAiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+//    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
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DiarAiTheme {
+        Greeting("Android")
+    }
+}
